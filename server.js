a7ccc6d86f37b45d39ae0900b035a19882db13b4
Yash Jadhav <yashjadhav7000@gmail.com>
2024-07-16 03:04:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mssql = require("mssql");
 const http = require("http");
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
