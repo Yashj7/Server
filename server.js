423e849cc9d4c78cb56c93d16bad70f8811b087c
Yash Jadhav <yashjadhav7000@gmail.com>
2024-07-16 03:11:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 
